bdb6e8fb64792f54ee427de45a0a388ad526fcc1
TakahashiJumpei <b1014098@gmail.com>
2022-06-19 01:19:15 +0900

    実装｜利用規約ページの実装

--- src/components/Tos.vue
+++ src/components/Tos.vue
@@ -1,7 +1,243 @@
 <template>
   <div class="main">
     <div class="inner">
-      利用規約ページ
+      <h1 class="title">サービス利用規約</h1>
+      <div class="tos-container">
+        <div class="tos-explanation-container">
+          <p class="tos-explanation">
+            本利用規約(以下、「本規約」)は、Seisan-kun
+            管理人(以下、「管理人」とします)がこのウェブサイト上で提供するSeisan-kun(以下、「本サービス」)の利用条件を定めるものです。登録ユーザーの皆様(以下、「ユーザー」)には、本規約に従って、本サービスをご利用いただきます。
+          </p>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第１条（適用）</h2>
+          <ul class="tos-item-explanation">
+            <li class="flex">
+              <p class="index">1.</p>
+              <p class="explanation">
+                本規約は，ユーザーと管理人との間の本サービスの利用に関わる一切の関係に適用されるものとします。
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">2.</p>
+              <p class="explanation">
+                管理人は本サービスに関し，本規約のほか，ご利用にあたってのルール等，各種の定め（以下，「個別規定」といいます。）をすることがあります。これら個別規定はその名称のいかんに関わらず，本規約の一部を構成するものとします。
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">3.</p>
+              <p class="explanation">
+                本規約の規定が前条の個別規定の規定と矛盾する場合には，個別規定において特段の定めなき限り，個別規定の規定が優先されるものとします。
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第２条（禁止事項）</h2>
+          <ul class="tos-item-explanation">
+            <li>
+              ユーザーは，本サービスの利用にあたり，以下の行為をしてはなりません。
+            </li>
+            <li class="flex">
+              <p class="index">1.</p>
+              <p class="explanation">法令または公序良俗に違反する行為</p>
+            </li>
+            <li class="flex">
+              <p class="index">2.</p>
+              <p class="explanation">犯罪行為に関連する行為</p>
+            </li>
+            <li class="flex">
+              <p class="index">3.</p>
+              <p class="explanation">
+                本サービスの内容等，本サービスに含まれる著作権，商標権ほか知的財産権を侵害する行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">4.</p>
+              <p class="explanation">
+                管理人，ほかのユーザー，またはその他第三者のサーバーまたはネットワークの機能を破壊したり，妨害したりする行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">5.</p>
+              <p class="explanation">
+                本サービスによって得られた情報を商業的に利用する行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">6.</p>
+              <p class="explanation">
+                管理人のサービスの運営を妨害するおそれのある行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">7.</p>
+              <p class="explanation">
+                不正アクセスをし，またはこれを試みる行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">8.</p>
+              <p class="explanation">
+                他のユーザーに関する個人情報等を収集または蓄積する行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">9.</p>
+              <p class="explanation">
+                不正な目的を持って本サービスを利用する行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">10.</p>
+              <p class="explanation">
+                本サービスの他のユーザーまたはその他の第三者に不利益，損害，不快感を与える行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">11.</p>
+              <p class="explanation">他のユーザーに成りすます行為</p>
+            </li>
+            <li class="flex">
+              <p class="index">12.</p>
+              <p class="explanation">
+                管理人が許諾しない本サービス上での宣伝，広告，勧誘，または営業行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">13.</p>
+              <p class="explanation">
+                面識のない異性との出会いを目的とした行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">14.</p>
+              <p class="explanation">
+                本サービスに関連して，反社会的勢力に対して直接または間接に利益を供与する行為
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">15.</p>
+              <p class="explanation">その他，管理人が不適切と判断する行為</p>
+            </li>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第３条（本サービスの提供の停止等）</h2>
+          <ul class="tos-item-explanation">
+            <li class="flex">
+              <p class="index">1.</p>
+              <p class="explanation">
+                管理人は，以下のいずれかの事由があると判断した場合，ユーザーに事前に通知することなく本サービスの全部または一部の提供を停止または中断することができるものとします。
+              </p>
+            </li>
+            <ul class="tos-item-sub-explanation">
+              <li class="flex">
+                <p class="index">1.</p>
+                <p class="explanation">
+                  本サービスにかかるコンピュータシステムの保守点検または更新を行う場合
+                </p>
+              </li>
+              <li class="flex">
+                <p class="index">2.</p>
+                <p class="explanation">
+                  地震，落雷，火災，停電または天災などの不可抗力により，本サービスの提供が困難となった場合
+                </p>
+              </li>
+              <li class="flex">
+                <p class="index">3.</p>
+                <p class="explanation">
+                  コンピュータまたは通信回線等が事故により停止した場合
+                </p>
+              </li>
+              <li class="flex">
+                <p class="index">4.</p>
+                <p class="explanation">
+                  その他，管理人が本サービスの提供が困難と判断した場合
+                </p>
+              </li>
+            </ul>
+            <li class="flex">
+              <p class="index">2.</p>
+              <p class="explanation">
+                管理人は，本サービスの提供の停止または中断により，ユーザーまたは第三者が被ったいかなる不利益または損害についても，一切の責任を負わないものとします。
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第４条（保証の否認および免責事項）</h2>
+          <ul class="tos-item-explanation">
+            <li class="flex">
+              <p class="index">1.</p>
+              <p class="explanation">
+                管理人は，本サービスに事実上または法律上の瑕疵（安全性，信頼性，正確性，完全性，有効性，特定の目的への適合性，セキュリティなどに関する欠陥，エラーやバグ，権利侵害などを含みます。）がないことを明示的にも黙示的にも保証しておりません。
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">2.</p>
+              <p class="explanation">
+                管理人は，本サービスに起因してユーザーに生じたあらゆる損害について一切の責任を負いません。ただし，本サービスに関する管理人とユーザーとの間の契約（本規約を含みます。）が消費者契約法に定める消費者契約となる場合，この免責規定は適用されません。
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">3.</p>
+              <p class="explanation">
+                前項ただし書に定める場合であっても，管理人は，管理人の過失（重過失を除きます。）による債務不履行または不法行為によりユーザーに生じた損害のうち特別な事情から生じた損害（管理人またはユーザーが損害発生につき予見し，または予見し得た場合を含みます。）について一切の責任を負いません。また，管理人の過失（重過失を除きます。）による債務不履行または不法行為によりユーザーに生じた損害の賠償は，ユーザーから当該損害が発生した月に受領した利用料の額を上限とします。
+              </p>
+            </li>
+            <li class="flex">
+              <p class="index">4.</p>
+              <p class="explanation">
+                管理人は，本サービスに関して，ユーザーと他のユーザーまたは第三者との間において生じた取引，連絡または紛争等について一切責任を負いません。
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第５条（サービス内容の変更等）</h2>
+          <ul class="tos-item-explanation">
+            <p>
+              管理人は，ユーザーに通知することなく，本サービスの内容を変更しまたは本サービスの提供を中止することができるものとし，これによってユーザーに生じた損害について一切の責任を負いません。
+            </p>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第６条（利用規約の変更）</h2>
+          <ul class="tos-item-explanation">
+            <p>
+              管理人は，必要と判断した場合には，ユーザーに通知することなくいつでも本規約を変更することができるものとします。なお，本規約の変更後，本サービスの利用を開始した場合には，当該ユーザーは変更後の規約に同意したものとみなします。
+            </p>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第７条（通知または連絡）</h2>
+          <ul class="tos-item-explanation">
+            <p>
+              ユーザーと管理人との間の通知または連絡は，管理人の定める方法によって行うものとします。管理人は,ユーザーから,管理人が別途定める方式に従った変更届け出がない限り,現在登録されている連絡先が有効なものとみなして当該連絡先へ通知または連絡を行い,これらは,発信時にユーザーへ到達したものとみなします。
+            </p>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第８条（権利義務の譲渡の禁止）</h2>
+          <ul class="tos-item-explanation">
+            <p>
+              ユーザーは，管理人の書面による事前の承諾なく，利用契約上の地位または本規約に基づく権利もしくは義務を第三者に譲渡し，または担保に供することはできません。
+            </p>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <h2 class="tos-item-title">第９条（準拠法・裁判管轄）</h2>
+          <ul class="tos-item-explanation">
+            <p>本規約の解釈にあたっては，日本法を準拠法とします。</p>
+          </ul>
+        </div>
+        <div class="tos-item-container">
+          <ul class="tos-item-explanation">
+            <p>以上</p>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,10 +248,76 @@
 
 .main {
   min-height: calc(100vh - #{$header-h} - #{$footer-h});
-  padding: $padding-tb $padding-lr;
+  padding: 0 $padding-lr $padding-tb $padding-lr;
   @media screen and(min-width: $min-width) {
     @import "../scss/breakpoints/768up";
-    padding: $padding-tb $padding-lr;
+    padding: 0 $padding-lr $padding-tb $padding-lr;
+  }
+  .inner {
+    .title {
+      border-top: 1px solid white;
+      margin-bottom: 10px;
+      font-size: 150%;
+      background-color: $green;
+      color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 20px;
+      padding: 10px;
+      margin-left: -$padding-lr;
+      margin-right: -$padding-lr;
+      margin-bottom: 20px;
+      @media screen and(min-width: $min-width) {
+        @import "../scss/breakpoints/768up";
+        margin-left: -$padding-lr;
+        margin-right: -$padding-lr;
+        font-size: 32px;
+      }
+    }
+    .tos-container {
+      display: flex;
+      flex-direction: column;
+      color: $base_text_color;
+      .tos-explanation-container {
+        display: flex;
+        font-size: 70%;
+        text-align: left;
+        border-bottom: 1px dotted #2c3e50;
+        margin-bottom: 20px;
+        padding-bottom: 10px;
+      }
+    }
+    .tos-item-container {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      margin-bottom: 10px;
+      li {
+        list-style: none;
+      }
+      .flex {
+        display: flex;
+      }
+      .tos-item-title {
+        font-size: 100%;
+      }
+      .tos-item-explanation {
+        font-size: 70%;
+        display: flex;
+        flex-direction: column;
+        padding: 0 15px;
+        margin-top: 10px;
+        .index {
+          display: inline-block;
+          margin-right: 4px;
+        }
+        .tos-item-sub-explanation {
+          padding: 0 15px;
+          margin: 10px 0;
+        }
+      }
+    }
   }
 }
 </style>
